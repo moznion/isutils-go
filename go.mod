b7b3d3cc08c27db01207dc15ea92e7717c008803
moznion <moznion@mail.moznion.net>
2023-11-09 09:21:45 +0900

    Add goccy-json serializer for gorilla securecookie
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module F
 
 go 1.21.1
+
+require (
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
+)
---
+++ gorilla/sucurecookie/goccy_json_serializer.go
@@ -0,0 +1,40 @@
+package sucurecookie
+
+import (
+	"fmt"
+
+	"github.com/goccy/go-json"
+	"github.com/gorilla/sessions"
+)
+
+type GoccyJSONSerializer struct {
+}
+
+// Serialize to JSON. Will err if there are unmarshalable key values
+func (s GoccyJSONSerializer) Serialize(ss *sessions.Session) ([]byte, error) {
+	m := make(map[string]interface{}, len(ss.Values))
+	for k, v := range ss.Values {
+		ks, ok := k.(string)
+		if !ok {
+			err := fmt.Errorf("Non-string key value, cannot serialize session to JSON: %v", k)
+			fmt.Printf("redistore.JSONSerializer.serialize() Error: %v", err)
+			return nil, err
+		}
+		m[ks] = v
+	}
+	return json.Marshal(m)
+}
+
+// Deserialize back to map[string]interface{}
+func (s GoccyJSONSerializer) Deserialize(d []byte, ss *sessions.Session) error {
+	m := make(map[string]interface{})
+	err := json.Unmarshal(d, &m)
+	if err != nil {
+		fmt.Printf("redistore.JSONSerializer.deserialize() Error: %v", err)
+		return err
+	}
+	for k, v := range m {
+		ss.Values[k] = v
+	}
+	return nil
+}
